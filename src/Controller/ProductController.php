0fdef5c4d40ec9c83ff2daea9d3e1cebfcdbfd7c
Marek <marek-bartocha@hotmail.com>
2025-03-17 15:57:29 +0100

    finish crudr

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -57,4 +57,50 @@ final class ProductController extends AbstractController
             'form' => $form
         ]);
     }
+
+
+    #[Route('/product/{id<\d+>}/edit', name: 'product_edit')]
+    public function edit(Product $product, Request $request, EntityManagerInterface $manager): Response {
+
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+           
+            $manager->flush();
+            $this->addFlash(
+                'notice',
+                'Product upsated successfully!'
+            );
+
+            
+            return $this->redirectToRoute('product_show', [
+                'id' => $product->getId()
+            ]);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/Product/{id<\d+>}/delete', name: 'product_delete')]
+    public function delete(Request $request, Product $product, EntityManagerInterface $manager): Response {
+
+        if ($request->isMethod('POST')) {
+            $manager->remove($product);
+            $manager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Product deleted successfully!'
+            );
+            
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('product/delete.html.twig', [
+            'id' => $product->getId()
+        ]);
+    }
 }
