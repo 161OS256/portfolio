062918e5b23dd06c36e2a1a8aea1846fa0ae4d47
Marek <marek-bartocha@hotmail.com>
2025-03-18 20:10:18 +0100

    Add multilingual support and project listing to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\VarDumper\Cloner\AbstractCloner;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,17 +15,38 @@ class HomeController extends AbstractController
     {
         // Zapisz język w sesji
         $request->getSession()->set('_locale', $_locale);
-
-        // Ustaw język aplikacji
         $request->setLocale($_locale);
 
         // Tłumaczenie
         $translatedText = $translator->trans('hello');
-        
-        // Przekazanie danych do szablonu
+
+        // Tablica projektów puzniej pociącnę to z bazy
+        $projects = [
+            'project1' => [
+                'name' => $translator->trans('project-01.title'),
+                'description' => $translator->trans('project-01.description'),
+                'src' => 'img/project-01.png', // Ścieżka do obrazka w public/img/
+                'route' => 'pascal',
+            ],
+            'project2' => [
+                'name' => $translator->trans('project-01.title'),
+                'description' => $translator->trans('project-02.description'),
+                'src' => 'img/project-01.png',
+                'route' => 'pascal',
+            ],
+            'project3' => [
+                'name' => $translator->trans('project-01.title'),
+                'description' => $translator->trans('project-03.description'),
+                'src' => 'img/project-01.png',
+                'route' => 'pascal',
+            ],
+        ];
+
+        // Przekazanie do szablonu
         return $this->render('home/index.html.twig', [
             'message' => $translatedText,
             'current_locale' => $_locale,
+            'projects' => $projects, // Dodajemy projekty
         ]);
     }
 }
--- src/Controller/PascalController.php
+++ src/Controller/PascalController.php
@@ -9,10 +9,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PascalController extends AbstractController
 {
     #[Route('{_locale}/pascal', name: 'pascal_index', requirements: ['_locale' => 'en|pl'])]
-    public function index(): Response
+    public function index(string $_locale): Response
     {
         return $this->render('pascal/index.html.twig', [
             'controller_name' => 'PascalController',
+            'current_locale' => $_locale,
         ]);
     }
 }
