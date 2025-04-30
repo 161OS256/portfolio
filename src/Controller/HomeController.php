15e07f16d8052d0ffefe077d0edbed647d1e00d7
Marek <marek-bartocha@hotmail.com>
2025-04-30 13:08:35 +0200

    rebuilding home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,43 +13,11 @@ class HomeController extends AbstractController
     #[Route('/{_locale}', name: 'home_index', requirements: ['_locale' => 'en|pl'])]
     public function index(TranslatorInterface $translator, Request $request, string $_locale): Response
     {
-        // Zapisz język w sesji
         $request->getSession()->set('_locale', $_locale);
         $request->setLocale($_locale);
 
-        // Tłumaczenie
-        $translatedText = $translator->trans('repo');
-
-        // Tablica projektów puzniej pociącnę to z bazy gdy będzie więcej projektów
-        $projects = [
-            'homeheating' => [
-                'route' => 'Home-heating',
-                'using' => 'Pascal, HTML, CSS, JavaScript',
-            ],
-            'organizerofthework' => [
-                'route' => 'organizer-of-the-work',
-                'using' => 'PHP, HTML, CSS, JavaScript, MySql',
-            ],
-            'volunteerassociation' => [
-                'route' => 'volunteer-association',
-                'using' => 'PHP, HTML, CSS, JavaScript, MySql',
-            ],
-            'monitoringwhewatchmanswork' => [
-                'route' => 'monitoring-whe-watchmans-work',
-                'using' => 'Pascal, HTML, CSS',
-            ],
-            'informationaboutthissite' => [
-                'route' => 'information-about-this-site',
-                'using' => 'Symfony',
-            ],
-
-        ];
-
-        // Przekazanie do szablonu
         return $this->render('home/index.html.twig', [
-            'message' => $translatedText,
             'current_locale' => $_locale,
-            'projects' => $projects, // Dodajemy projekty
             'site' => '',
         ]);
     }
